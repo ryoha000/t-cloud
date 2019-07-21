c58db826e893bcae7f309947a34ff280d1198ce2
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2019-07-22 04:25:56 +0900

    a

--- main.go
+++ main.go
@@ -281,7 +281,7 @@ func getGameInfoHandler(c echo.Context) error {
 		return c.NoContent(http.StatusNotFound)
 	}
 
-	return c.JSON(http.StatusOK, game)
+	return c.JSON(http.StatusOK, game.GameName)
 }
 
 // func rightButtonHandler(c echo.Context) error {
